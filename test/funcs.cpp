269800e28c56b1498b62a28ac81027f6e1575454
zxfishhack <zxfishhack@126.com>
2024-04-18 14:32:52 +0800

    完善cmake项目

--- test/funcs.cpp
+++ test/funcs.cpp
@@ -4,10 +4,23 @@
 
 #include <stdio.h>
 #include <stdint.h>
+
+#ifdef WIN32
 #include <sysinfoapi.h>
+#else
+#include <sys/time.h>
+#endif
 
 extern "C" uint32_t  GetTick() {
+#ifdef WIN32
   return GetTickCount();
+#else
+  struct timeval tv;
+  if (gettimeofday(&tv, NULL) == 0) {
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+  }
+#endif
+  return 0;
 }
 
 extern "C" int PutChar(char ch) {
--- test/test.cpp
+++ test/test.cpp
@@ -1,12 +1,13 @@
 //
 // Created by Fish on 2020-09-09.
 //
+#include <cstdio>
 #include "tiny_log.h"
-#include <stdio.h>
 
 int main() {
   log_info("test %ss %.2f", "123", 12.3);
   GetTick();
-  PutChar(12);
+  PutChar('1');
+  printf("\n");
   return 0;
 }
