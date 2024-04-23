e0dfb1a8296336539f28f96ce28c2b20ff2e8c0c
zxfishhack <zxfishhack@126.com>
2024-04-23 17:27:45 +0800

    BUGFIX: macro define mismatch

--- include/tiny_log.h
+++ include/tiny_log.h
@@ -12,7 +12,7 @@ extern "C" {
 #define LOG_ERROR   4
 #define LOG_FATAL   5
 #define LOG_NONE    6
-#include "config.h"
+#include "tinylog_config.h"
 #include <stdint.h>
 
 uint32_t __attribute((weak)) GetTick();
@@ -22,37 +22,37 @@ void __attribute((weak)) TinyLogLeaveHook();
 
 void tiny_log(int logLevel, const char* fmt, ...);
 
-#if LOG_LEVEL <= LOG_DEBUG
+#if TINY_LOG_LOG_LEVEL <= LOG_DEBUG
 #define log_debug(...) tiny_log(LOG_DEBUG, __VA_ARGS__)
 #else
 #define log_debug(...)
 #endif // LOG_DEBUG
 
-#if LOG_LEVEL <= LOG_VERBOSE
+#if TINY_LOG_LOG_LEVEL <= LOG_VERBOSE
 #define log_verbose(...) tiny_log(LOG_VERBOSE, __VA_ARGS__)
 #else
 #define log_verbose(...)
 #endif // LOG_VERBOSE
 
-#if LOG_LEVEL <= LOG_INFO
+#if TINY_LOG_LOG_LEVEL <= LOG_INFO
 #define log_info(...) tiny_log(LOG_INFO, __VA_ARGS__)
 #else
 #define log_info(...)
 #endif // LOG_INFO
 
-#if LOG_LEVEL <= LOG_WARNING
+#if TINY_LOG_LOG_LEVEL <= LOG_WARNING
 #define log_warning(...) tiny_log(LOG_WARNING, __VA_ARGS__)
 #else
 #define log_warning(...)
 #endif // LOG_WARNING
 
-#if LOG_LEVEL <= LOG_ERROR
+#if TINY_LOG_LOG_LEVEL <= LOG_ERROR
 #define log_error(...) tiny_log(LOG_ERROR, __VA_ARGS__)
 #else
 #define log_error(...)
 #endif // LOG_ERROR
 
-#if LOG_LEVEL <= LOG_FATAL
+#if TINY_LOG_LOG_LEVEL <= LOG_FATAL
 #define log_fatal(...) tiny_log(LOG_FATAL, __VA_ARGS__)
 #else
 #define log_fatal(...)
